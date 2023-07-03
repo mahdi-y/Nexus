4525a39680288b8ce9a44e902a2e19f570197c36
youssefazzouz2 <youssefazzouz2@gmail.com>
2023-07-03 11:10:40 +0100

    fixed some imports

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,11 +12,9 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 use App\Entity\Utilisateur;
 use DateTimeImmutable;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 
 
@@ -92,6 +90,5 @@ class SecurityController extends AbstractController
         }
 
         return $this->render('security/register.html.twig');
-
- }
+    }
 }
--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Form
+namespace App\Form;
+
 use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;

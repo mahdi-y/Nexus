bec1d5094692d49ebebe76da05c59a6bb9f448a6
Mohamed Aziz <mohamedaziz.guerbouj@esprit.tn>
2023-07-13 16:08:40 +0100

    minor changes!

--- src/Controller/MailerService.php
+++ src/Controller/MailerService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Utilisateur;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
@@ -8,10 +9,12 @@ use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mime\Address;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpClient\HttpClient;
 
 
 
@@ -23,7 +26,7 @@ use Symfony\Component\Routing\RouterInterface;
 
 use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
 
-class MailerService 
+class MailerService
 {
     private $mailer;
     private $urlGenerator;
@@ -34,19 +37,28 @@ class MailerService
         $this->urlGenerator = $urlGenerator;
     }
 
- public function sendConfirmationEmail($recipientEmail, $nomU)
+    public function sendConfirmationEmail($recipientEmail, $nomU)
     {
+        $console = new ConsoleOutput();
+        $output = [];
+        exec("ipconfig", $output);
+        $jsonString = implode('', $output); // Convert array to a single string
+        $lol = json_decode($jsonString, true); // Decode the JSON string into an array
+        $console->writeln($lol);
+        foreach ($output as $item) {
+        }
         // Create the Gmail SMTP transport
         $transport = new EsmtpTransport('smtp.gmail.com', 587);
         $transport->setUsername('youssef.azzouz@esprit.tn');
         $transport->setPassword('ncolrbbujrynyerw'); // Replace with your app-specific password
-    
+
         // Create the mailer instance
         $mailer = new Mailer($transport);
 
-        $baseURL = 'https://10.1.143.167:8000';
+        //$baseURL = 'https://10.1.143.167:8000';
+        $baseURL = $lol;
         $confirmationLink = $baseURL . $this->urlGenerator->generate('confirm_email', ['email' => $recipientEmail], UrlGeneratorInterface::ABSOLUTE_PATH);
-             
+
 
 
         // Create the email
@@ -55,27 +67,39 @@ class MailerService
             ->addFrom($sender)
             ->to($recipientEmail)
             ->subject('Confirmation Email')
-            ->text('Dear ' . $nomU . ', thank you for registering. Please confirm your email.'. $confirmationLink);
-    
+            ->text('Dear ' . $nomU . ', thank you for registering. Please confirm your email.' . $confirmationLink);
+
         // Send the email
         $mailer->send($email);
-    
+
         // Return a response or perform other actions
         return new Response('Confirmation email sent!');
     }
     public function sendReset($recipientEmail)
     {
+        $console = new ConsoleOutput();
+        $output = [];
+        $command = "ipconfig /all | findstr /I 10.1.14 | Select -First 1";
+        //exec("ipconfig", $output);
+        exec($command, $output);
+        //exec(' -split ': '')
+        $jsonString = implode('', $output); // Convert array to a single string
+        $lol = json_decode($jsonString, true); // Decode the JSON string into an array
+        $console->writeln($lol);
+        foreach ($output as $item) {
+        }
         // Create the Gmail SMTP transport
         $transport = new EsmtpTransport('smtp.gmail.com', 587);
         $transport->setUsername('youssef.azzouz@esprit.tn');
         $transport->setPassword('ncolrbbujrynyerw'); // Replace with your app-specific password
-    
+
         // Create the mailer instance
         $mailer = new Mailer($transport);
 
-        $baseURL = 'https://10.1.143.167:8000';
-    $confirmationLink = $baseURL . $this->urlGenerator->generate('user.edit.password1', ['email' => $recipientEmail], UrlGeneratorInterface::ABSOLUTE_PATH);
-             
+        //$baseURL = 'https://10.1.143.167:8000';
+        $baseURL = $lol;
+        $confirmationLink = $baseURL . $this->urlGenerator->generate('user.edit.password1', ['email' => $recipientEmail], UrlGeneratorInterface::ABSOLUTE_PATH);
+
 
 
         // Create the email
@@ -84,14 +108,12 @@ class MailerService
             ->addFrom($sender)
             ->to($recipientEmail)
             ->subject('Confirmation Email')
-            ->text('Click this link to select your new password '. $confirmationLink);
-    
+            ->text('Click this link to select your new password ' . $confirmationLink);
+
         // Send the email
         $mailer->send($email);
-    
+
         // Return a response or perform other actions
         return new Response('Confirmation email sent!');
     }
- 
 }
-
